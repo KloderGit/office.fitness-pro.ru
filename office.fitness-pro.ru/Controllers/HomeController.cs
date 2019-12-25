84bfbb09517396f8b8c84156f28d447586642943
KloderGit <idzhyan.develop@outlook.com>
2019-12-25 16:43:19 +0300

    chg version to .net core 2.2 add appVeyor config

--- office.fitness-pro.ru/Controllers/HomeController.cs
+++ office.fitness-pro.ru/Controllers/HomeController.cs
@@ -2,82 +2,17 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Net.Http;
 using System.Threading.Tasks;
-using amocrm.library;
-using amocrm.library.Interfaces;
-using crm.service.database;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using office.fitness_pro.ru.Models;
 
 namespace office.fitness_pro.ru.Controllers
 {
-
-    public partial class IpInfo
-    {
-        public string ip { get; set; }
-        public bool success { get; set; }
-        public string type { get; set; }
-        public string continent { get; set; }
-        public string continent_code { get; set; }
-        public string country { get; set; }
-        public string country_code { get; set; }
-        public string country_flag { get; set; }
-        public string country_capital { get; set; }
-        public string country_phone { get; set; }
-        public string country_neighbours { get; set; }
-        public string region { get; set; }
-        public string city { get; set; }
-        public string latitude { get; set; }
-        public string longitude { get; set; }
-        public string asn { get; set; }
-        public string org { get; set; }
-        public string isp { get; set; }
-        public string timezone { get; set; }
-        public string timezone_name { get; set; }
-        public string timezone_dstOffset { get; set; }
-        public string timezone_gmtOffset { get; set; }
-        public string timezone_gmt { get; set; }
-        public string currency { get; set; }
-        public string currency_code { get; set; }
-        public string currency_symbol { get; set; }
-        public string currency_rates { get; set; }
-        public string currency_plural { get; set; }
-        public int completed_requests { get; set; }
-    }
-
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-        CrmDataBaseContext crmDataBase;
-        ICrmManager crm;
-
-        public HomeController(ILogger<HomeController> logger, CrmDataBaseContext crmDataBase, ICrmManager crm)
+        public IActionResult Index()
         {
-            _logger = logger;
-            this.crmDataBase = crmDataBase;
-            this.crm = crm;
-        }
-
-        public async Task<IActionResult> Index()
-        {
-            var clientUA = new HttpClient();
-
-            var ress = await clientUA.GetAsync(@"http://free.ipwhois.io/json/195.239.87.70?lang=ru");
-
-            var dfff = await ress.Content.ReadAsAsync<IpInfo>();
-
-            var client = new HttpClient();
-            var request = await client.GetAsync(@"https://lc.fitness-pro.ru/Program/ProgramAnnotation").ConfigureAwait(false);
-
-            var lcProgs = await request.Content.ReadAsAsync<IEnumerable<EventsDto>>();
-
-
-            var task = await crm.CustomFields.ConfigureAwait(false);
-            var result = task.Lead[66349].Enums;
-
-            return View( new VM { Amo = result, lC = lcProgs });
+            return View();
         }
 
         public IActionResult Privacy()
@@ -92,9 +27,8 @@ namespace office.fitness_pro.ru.Controllers
         }
     }
 
-
     public class VM
-    { 
+    {
         public Dictionary<int, string> Amo { get; set; }
         public IEnumerable<EventsDto> lC { get; set; }
     }
--- office.fitness-pro.ru/Startup.cs
+++ office.fitness-pro.ru/Startup.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,35 +7,41 @@ using amocrm.library.Interfaces;
 using crm.service.database;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
 namespace office.fitness_pro.ru
 {
     public class Startup
     {
-        public IConfiguration Configuration { get; }
-
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
             services.AddDbContext<CrmDataBaseContext>(optition =>
                         optition.UseNpgsql(Configuration.GetSection("ConnectionStrings:CrmDataBase").Value));
 
-            services.AddScoped<ICrmManager>(crm => new CrmManager("","",""));
+            services.AddScoped<ICrmManager>(crm => new CrmManager(
+                Configuration.GetSection("AmoCrm:account").Value,
+                Configuration.GetSection("AmoCrm:login").Value,
+                Configuration.GetSection("AmoCrm:hash").Value));
+
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -47,30 +53,20 @@ namespace office.fitness_pro.ru
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseCookiePolicy();
 
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            //app.UseEndpoints(endpoints =>
-            //{
-            //    endpoints.MapControllerRoute(
-            //        name: "default",
-            //        pattern: "{controller=Home}/{action=Index}/{id?}");
-            //});
-
-            app.UseEndpoints(endpoints =>
+            app.UseMvc(routes =>
             {
-                endpoints.MapControllerRoute(
-                    name: "MyArea",
-                  pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
-                endpoints.MapControllerRoute(
+                routes.MapRoute(
+                    name: "area",
+                    template: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+                routes.MapRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Home}/{action=Index}/{id?}");
             });
-
         }
     }
 }
